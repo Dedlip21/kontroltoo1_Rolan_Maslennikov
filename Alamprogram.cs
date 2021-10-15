544461119a1f71aff878109844d822a3d6cb0f43
Opilane TTHK <opilane@tthk.ee>
2021-10-15 15:51:31 +0300

    Add project files.

---
+++ Alamprogram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kontroltoo1_Rolan_Maslennikov
+{
+    public class Alamprogram
+    {
+        public static void Min()
+        {
+            Random rnd = new Random();
+            int num1 = rnd.Next(1, 10);
+            int num2 = rnd.Next(1, 10);
+            int minNum;
+
+            Console.WriteLine("Number 1 - " + num1);
+            Console.WriteLine("Number 2 - " + num2);
+
+            if (num1 < num2)
+            {
+                minNum = num1;
+            }
+            else
+            {
+                minNum = num2;
+            }
+            Console.WriteLine("Minimum number is - " + minNum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace kontroltoo1_Rolan_Maslennikov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // ülesanne 1
+            Console.WriteLine("Say 1 to see maximum number\nSay 2 to see minimum number");
+            
+            int ch = int.Parse(Console.ReadLine());
+
+            switch (ch)
+            {
+                case 1:
+                    Alamprogram.Max();
+                    break;
+                case 2:
+                    Alamprogram.Min();
+                    break;
+            }
+            Console.WriteLine(" ");
+
+
+            Console.ReadLine();
+        }
+    }
+}
